3a2092d884c2cf6a63800581c68acd8e056c8e70
Deepacks <vladimirsette.vc@gmail.com>
2020-10-05 22:44:28 +0200

    v1.3
    + Aggiute le operazioni Potenza e Radice

--- Calc.cpp
+++ Calc.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(void) {
-  cout << "Somma: 1    Differenza: 2    Moltiplicazione: 3   Divisione: 4\n" << endl ;
-    int op, n1, n2 ;
+  cout << "Somma: 1    Differenza: 2    Moltiplicazione: 3    Divisione: 4    Potenza: 5    Radice: 6 \n" << endl ;
+  int op ;
+  int n1, n2 ;
   int flag = 0 ;
 
   do{
     if (flag > 0) {
-      cout <<"\nSono accettati valori da 1 a 4" <<endl ;
+      cout <<"\nSono accettati valori da 1 a 6" <<endl ;
     }
     cout << "Inserire il tipo di operazione: " ;
     cin >> op ;
@@ -16,12 +18,14 @@ int main(void) {
     if (op == 2) cout << "\n-Differenza-\n" << endl ;
     if (op == 3) cout << "\n-Moltiplicazione-\n" << endl ;
     if (op == 4) cout << "\n-Divisione-\n" << endl ;
+    if (op == 5) cout << "\n-Potenza-\n" << endl ;
+    if (op == 6) cout << "\n-Radice-\n" << endl ;
     flag++ ;
-  }while (op > 4) ;
+  }while (op > 6) ;
 
 //Somma
   if (op == 1) {
-    cout << "Inserisci i due numeri da Sommare: "<< endl ;
+    cout << "Inserisci i due numeri da Sommare: " << endl ;
     cout << "Primo numero: " ;
     cin >> n1 ;
     cout << "Secondo numero: " ;
@@ -32,7 +36,7 @@ int main(void) {
 
 //Differenza
   if (op == 2) {
-    cout << "Inserisci i due numeri da Sottrarre: "<< endl ;
+    cout << "Inserisci i due numeri da Sottrarre: " << endl ;
     cout << "Primo numero: " ;
     cin >> n1 ;
     cout << "Secondo numero: " ;
@@ -43,7 +47,7 @@ int main(void) {
 
 //Moltiplicazione
   if (op == 3) {
-    cout << "Inserisci i due numeri da Moltiplicare: "<< endl ;
+    cout << "Inserisci i due numeri da Moltiplicare: " << endl ;
     cout << "Primo numero: " ;
     cin >> n1 ;
     cout << "Secondo numero: " ;
@@ -55,7 +59,7 @@ int main(void) {
 //Divisione
   if (op == 4) {
     float res = 0 ;
-    cout << "Inserisci i due numeri da Moltiplicare: "<< endl ;
+    cout << "Inserisci i due numeri da Moltiplicare: " << endl ;
     cout << "Primo numero: " ;
     cin >> n1 ;
     cout << "Secondo numero: " ;
@@ -65,6 +69,26 @@ int main(void) {
     res = res / n2 ;
     res = res + op ;
     cout << "\n" << n1 << " / " << n2 << " = " << res ;
+  }
+
+//Potenza
+  if (op == 5) {
+    cout << "Inserisci la Base e l'Esponente: " << endl ;
+    cout << "Base: " ;
+    cin >> n1 ;
+    cout << "Esponente: " ;
+    cin >> n2 ;
+    op = pow(n1, n2) ;
+    cout << "\n" << n1 << " ^ " << n2 << " = " << op ;
+  }
+
+//Radice
+  if(op == 6) {
+    double rad ;
+    cout << "Inserisci il numero: " << endl ;
+    cin >> n1 ;
+    rad = sqrt(n1) ;
+    cout << "\n" << "Radice (" << n1 << ") = " << rad ;
   }
 
   return 0 ;
